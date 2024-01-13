f9611fcc65be34100d816c957304c4a8d824bfd2
GauravKapil <gauravcapil@gmail.com>
2024-01-13 21:12:23 +0530

    Adding appropriate test query for get list of tiger
    Signed-off-by: GauravKapil <gauravcapil@gmail.com>

--- e2eTests/get_latest_sightings_list.sh
+++ e2eTests/get_latest_sightings_list.sh
@@ -1,2 +1,2 @@
 source ../config.cfg
-psql -h localhost -U $PGUSER -d postgres -c "select * from (select distinct on (s.tiger_id) * from sightings s order by s.tiger_id, s.seen_at desc) join tiger_data t on t.id = tiger_id;"
+psql -h localhost -U $PGUSER -d postgres -c "select tiger_id,seen_at,seen_at_lat,seen_at_lon, photo_location, user_name, name, date_of_birth from (select distinct on (s.tiger_id) * from sightings s order by s.tiger_id, s.seen_at desc) join tiger_data t on t.id = tiger_id limit $1 offset $2;"
